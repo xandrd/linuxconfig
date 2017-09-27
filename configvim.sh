1546e8554bf3c697cf639f5c9972b9bf247734e4
Alexander Drozdov <adrozdov@ucla.edu>
2017-09-26 17:15:43 -0700

    Vim configuration script added

---
+++ configvim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TARGETFILE=$HOME/.vimrc
+
+# create file if not exist
+[ ! -f $TARGETFILE ] && touch $TARGETFILE || echo ".vimrc Found"
+
+
+# read data from the file
+while read -r configline ; do
+	#trim line
+	line="${configline##*( )}"
+	line="${configline%%*( )}"
+	# skip empty lines
+	[ -z "$configline" ] && continue
+	# skip comments
+	[[ ${configline:0:1} == "#" ]] && continue
+
+	# now we search the config file
+	if grep -q "$configline" $TARGETFILE ; then
+		echo "Settings [$configline] already exist. Skip."
+	else
+		echo $configline >> $TARGETFILE
+	fi
+
+done<vim.config
